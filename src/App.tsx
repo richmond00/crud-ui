078839079b0596461b1c263a022b01e61b991b00
Dongwook Kim <dongwook227@gmail.com>
2025-04-20 14:38:27 -0400

    test husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React from "react";
 
 const App = () => {
+    console.log("husky test  " );
+
+
     return <div>hello world</div>
-};
+}
 
 export default App;
