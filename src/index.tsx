129ad7b09d4882c86a107069f2d2bb19a2a11157
Dongwook Kim <dongwook227@gmail.com>
2025-04-24 06:45:47 -0400

    set up query client

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
-root.render(<App />);
+
+root.render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
