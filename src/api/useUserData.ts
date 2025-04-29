3b3a88c7d4180c5e117e4d1b09bd886bb63e4850
Dongwook Kim <dongwook227@gmail.com>
2025-04-29 06:27:25 -0400

    add type for user data

--- src/api/useUserData.ts
+++ src/api/useUserData.ts
@@ -1,8 +1,14 @@
 import axios from 'axios';
 import { useQuery } from '@tanstack/react-query';
 
+interface User {
+  id: string;
+  username: string;
+  email: string;
+}
+
 export const useUserData = () =>
-  useQuery({
+  useQuery<User[], Error>({
     queryKey: ['users'],
     queryFn: async () => {
       const { data } = await axios.get('http://localhost:8080/api/users');
