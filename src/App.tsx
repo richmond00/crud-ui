fd170e92c3cee4f41c5341389c65ffea646a53bf
Dongwook Kim <dongwook227@gmail.com>
2025-04-29 06:27:51 -0400

    render user list

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import UserList from './components/UserList';
 
 const App = () => {
-  console.log('husky test ');
-  return <div>hello world</div>;
+  return (
+    <div>
+      <UserList />
+    </div>
+  );
 };
 
 export default App;
