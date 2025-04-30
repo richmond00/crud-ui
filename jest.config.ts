1b23ffe51b051b062fc6b1b6f029fcabe02cc044
Dongwook Kim <dongwook227@gmail.com>
2025-04-30 06:55:05 -0400

    set up jest

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+// jest.config.ts
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/index.ts',
+  ],
+  coverageReporters: ['text', 'lcov', 'html'],
+};
+
+export default config;
