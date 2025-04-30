383227f6f88688255b4ba10c72b0b4e87e33950d
Dongwook Kim <dongwook227@gmail.com>
2025-04-30 07:04:58 -0400

    add first test

---
+++ src/App.test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+jest.mock('./components/UserList');
+
+test('page displays page title', () => {
+  render(<App />);
+
+  const title = screen.getByRole('heading', { level: 1 });
+
+  expect(title).toBeInTheDocument();
+  expect(title).toHaveTextContent('User list');
+});
