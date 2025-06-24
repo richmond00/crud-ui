c3c9b4cefc458b743d458fd511e5def2f263b407
Dongwook Kim <dongwook227@gmail.com>
2025-06-23 22:31:04 -0400

    change structure from list to table

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -8,15 +8,31 @@ const UserList = () => {
   if (error) return <div>Error: {error.message}</div>;
 
   return (
-    <ul>
-      {data?.map((user) => (
-        <li key={user.id}>
-          <div>
-            name: {user.username}, email: {user.email}
-          </div>
-        </li>
-      ))}
-    </ul>
+    // <ul>
+    //   {data?.map((user) => (
+    //     <li key={user.id}>
+    //       <div>
+    //         name: {user.username}, email: {user.email}
+    //       </div>
+    //     </li>
+    //   ))}
+    // </ul>
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data?.map((user) => (
+          <tr>
+            <td>{user.username}</td>
+            <td>{user.email}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
