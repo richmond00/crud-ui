b792e171f477d26ff9284b0eff41da6d1d35dd7a
Dongwook Kim <dongwook227@gmail.com>
2025-08-26 20:54:23 -0400

    remove host

--- msw/handlers.ts
+++ msw/handlers.ts
@@ -1,8 +1,7 @@
 import { http, HttpResponse } from 'msw';
-import { origin } from '../src/lib/const';
 
 export const handlers = [
-  http.get(`${origin}/api/users`, () => {
+  http.get('/api/users', () => {
     return HttpResponse.json([
       { id: '1', username: 'john', email: 'john@gmail.com' },
       { id: '2', username: 'kim', email: 'kim@gmail.com' },
